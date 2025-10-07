de9dc9d89b3cb56f794fd20c2a2ef3883ed2d316
Russs11 <russs.khalilov@gmail.com>
2025-10-07 22:44:39 +0300

    h07.10.2025

--- src/app/components/TableCard.tsx
+++ src/app/components/TableCard.tsx
@@ -15,19 +15,19 @@ export default function TableCard() {
         {
             key: "1",
             name: "1",
-            role: "Кража из а/м",
+            typeOfCrime: "Кража из а/м",
             status: "1234 от 11.09.2025",
         },
         {
             key: "2",
-            name: "Zoey Lang",
-            role: "Technical Lead",
-            status: "Paused",
+            name: "ОВД",
+            typeOfCrime: "Адрес",
+            status: "УД",
         },
         {
             key: "3",
             name: "Jane Fisher",
-            role: "Senior Developer",
+            typeOfCrime: "Senior Developer",
             status: "Active",
         },
 
@@ -39,7 +39,7 @@ export default function TableCard() {
             label: "№ по сводке",
         },
         {
-            key: "role",
+            key: "typeOfCrime",
             label: "Вид преступления",
         },
         {
@@ -55,32 +55,15 @@ export default function TableCard() {
             isStriped={true}
         // removeWrapper
         >
-            <TableHeader>
-                <TableColumn>NAME</TableColumn>
-                <TableColumn>ROLE</TableColumn>
-                <TableColumn>STATUS</TableColumn>
+            <TableHeader columns={columns}>
+                {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
             </TableHeader>
-            <TableBody>
-                <TableRow key="1">
-                    <TableCell>Tony Reichert</TableCell>
-                    <TableCell>CEO</TableCell>
-                    <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="2">
-                    <TableCell>Zoey Lang</TableCell>
-                    <TableCell>Technical Lead</TableCell>
-                    <TableCell>Paused</TableCell>
-                </TableRow>
-                <TableRow key="3">
-                    <TableCell>Jane Fisher</TableCell>
-                    <TableCell>Senior Developer</TableCell>
-                    <TableCell>Active</TableCell>
-                </TableRow>
-                <TableRow key="4">
-                    <TableCell>William Howard</TableCell>
-                    <TableCell>Community Manager</TableCell>
-                    <TableCell>Vacation</TableCell>
-                </TableRow>
+            <TableBody items={rows}>
+                {(item) => (
+                    <TableRow key={item.key}>
+                        {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                    </TableRow>
+                )}
             </TableBody>
         </Table>
     );
